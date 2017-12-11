65cbaec03f1fc71fdf21304f112856160dc50fd0
JTong <eldertong@gmail.com>
2017-12-11 12:54:27 -0500

    randomHole function getting built

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 const holes = document.querySelectorAll('.hole');
-        const scoreBoard = document.querySelector('.score');
-        const moles = document.querySelectorAll('.mole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
 
-        function randTime(max, min) {
-            return Math.round(Math.random() * (max - min) + min);
-        }
+function randTime(max, min) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+    console.log(holes.length);
+}
